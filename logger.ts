13321c32fac7b0646af26530e86551fc739ff0c6
Vladimir Sabev <vdsabev@gmail.com>
2017-05-13 15:28:10 +0300

    Rewrote logger to allow creating new loggers

--- logger.ts
+++ logger.ts
@@ -1,5 +1,22 @@
-export const logger = exports.logger = {
-  log: console.log,
-  error: console.error,
-  maybeError: (e: any) => e ? logger.error(e) : null
+export interface Logger extends LoggerOutput {
+  (output: LoggerOutput, ...args: any[]): Logger;
+  maybeError(e: any): void;
+}
+
+interface LoggerOutput {
+  log(...args: any[]): void;
+  error(...args: any[]): void;
+}
+
+const createLogger = (output: LoggerOutput, ...args: any[]): Logger => {
+  const logger = <Logger>createLogger;
+
+  logger.log = output.log.bind(null, ...args);
+  logger.error = output.error.bind(null, ...args);
+  logger.maybeError = (e: any) => e && logger.error ? logger.error(e) : null;
+
+  return logger;
 };
+
+// Default logger that outputs to the console
+export const logger = createLogger(console);
