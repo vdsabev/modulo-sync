4295ddf405512aedc0f1c32bd4cb73eda46d25e2
Vladimir Sabev <vdsabev@gmail.com>
2017-05-18 18:15:32 +0300

    Finished GCS store tests

--- stores/gcs.test.ts
+++ stores/gcs.test.ts
@@ -3,13 +3,56 @@ import 'jest';
 import * as path from 'path';
 jest.mock('path', () => ({ ...path, sep: '/' }));
 
-const bucket = jest.fn();
-jest.mock('@google-cloud/storage', () => jest.fn(() => ({ bucket })));
+import { constant } from '../utils';
+
+const readStream: any = {
+  push: jest.fn(),
+  pipe: jest.fn(() => readStream),
+  on: jest.fn(() => readStream)
+};
+jest.mock('stream', constant({ Readable: jest.fn(constant(readStream)) }));
+import { Readable } from 'stream';
+
+const fileResult: any = {
+  download: jest.fn(() => fileResult),
+  delete: jest.fn(() => fileResult),
+  catch: jest.fn(() => fileResult),
+  createWriteStream: jest.fn()
+};
+const file = jest.fn(constant(fileResult));
+jest.mock('@google-cloud/storage', () => jest.fn(constant({ bucket: constant({ file }) })));
 
 jest.mock('../logger', () => ({ logger: { log: jest.fn(), error: jest.fn() } }));
 import { logger } from '../logger';
 
 import { store } from './gcs';
+
+describe(`read`, () => {
+  it(`should read reference`, () => {
+    store.read('read');
+    expect(file).toHaveBeenLastCalledWith('read');
+    expect(file().download).toHaveBeenCalled();
+    expect(file().catch).toHaveBeenLastCalledWith(logger.error);
+  });
+});
+
+describe(`write`, () => {
+  it(`should create read stream and call bucket API methods`, () => {
+    store.write('write', 'content');
+    expect(file).toHaveBeenLastCalledWith('write');
+    expect(file().createWriteStream).toHaveBeenCalled();
+    expect(Readable).toHaveBeenCalled();
+  });
+});
+
+describe(`delete`, () => {
+  it(`should call bucket API methods`, () => {
+    store.delete('delete');
+    expect(file).toHaveBeenLastCalledWith('delete');
+    expect(file().delete).toHaveBeenCalled();
+    expect(file().catch).toHaveBeenLastCalledWith(logger.error);
+  });
+});
 
 describe(`watch`, () => {
   it(`should log action`, () => {
